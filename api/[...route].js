b17dc5f163ec9c29c8e88d6b461315550bfda4f0
keko <keko@PumilairdeMacBook-Air.local>
2025-09-29 18:44:13 +0800

    fix: export express app directly for vercel api

--- api/[...route].js
+++ api/[...route].js
@@ -1,14 +1,9 @@
-import { createServer } from 'http';
 import { app } from '../server/app.js';
 
 export const config = {
   api: {
     bodyParser: false,
-    externalResolver: true,
   },
 };
 
-export default function handler(req, res) {
-  const server = createServer(app);
-  server.emit('request', req, res);
-}
+export default app;
